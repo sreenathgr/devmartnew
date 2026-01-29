54381acd69721c5e86162062490b3652880e7b68
SREENATH G R <gr.sreenat@gmail.com>
2026-01-29 16:01:51 +0530

    login screen ui in progress

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,10 +1,76 @@
+import { Colors } from '@/constants/Colors';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={{ flex: 1, backgroundColor: Colors.loginBackground }}>
+      <View
+        style={{
+          backgroundColor: Colors.loginBackgroundSecondary,
+          borderColor: Colors.acccentBlue,
+          borderWidth: 1,
+          marginTop: 10,
+          marginHorizontal: 20,
+
+          paddingBottom: '20%',
+        }}
+      >
+        <View style={{ paddingTop: '30%', alignItems: 'center' }}>
+          <Text style={{ color: 'white', fontFamily: 'Manrope', fontSize: 30 }}>
+            Welcome Back
+          </Text>
+        </View>
+        <View style={{ paddingTop: '3%', alignItems: 'center' }}>
+          <Text style={{ color: 'white', fontFamily: 'Manrope', fontSize: 15 }}>
+            Enter your details to access your account
+          </Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: Colors.charcoal,
+            borderColor: Colors.acccentBlue,
+            borderWidth: 1,
+            marginTop: '3%',
+            marginHorizontal: '8%',
+            paddingBottom: '5%',
+          }}
+        >
+          <View style={{ paddingTop: '10%', paddingStart: '10%' }}>
+            <Text style={{ color: 'white', fontFamily: 'Manrope' }}>
+              EMAIL ADDRESS
+            </Text>
+          </View>
+          <View style={{ paddingHorizontal: '10%', paddingTop: '5%' }}>
+            <TextInput
+              style={{
+                height: 50,
+                backgroundColor: '#BDBDBD',
+                borderRadius: 20,
+                borderColor: Colors.acccentBlue,
+                borderWidth: 0.5,
+              }}
+            />
+          </View>
+          <View style={{ paddingTop: '10%', paddingStart: '10%' }}>
+            <Text style={{ color: 'white', fontFamily: 'Manrope' }}>
+              PASSWORD
+            </Text>
+          </View>
+          <View style={{ paddingHorizontal: '10%', paddingTop: '5%' }}>
+            <TextInput
+              style={{
+                height: 50,
+                backgroundColor: '#BDBDBD',
+                borderRadius: 20,
+                borderColor: Colors.acccentBlue,
+                borderWidth: 0.5,
+              }}
+              secureTextEntry
+            />
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,4 +3,8 @@ export const Colors = {
   onboardingBackground: '#1a203c',
   onboardingButton: 'purple',
   backgroundLight: '#f5f6f8',
+  loginBackground: '#0F172A',
+  loginBackgroundSecondary: '#1F2937',
+  acccentBlue: '#60A5FA',
+  charcoal: '#1F2937',
 };
