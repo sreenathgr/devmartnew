5edfd054616ac8e508a987a6640880d3d3f3838b
SREENATH G R <gr.sreenat@gmail.com>
2026-01-30 12:19:09 +0530

    completed login screen ui

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,75 +1,96 @@
 import { Colors } from '@/constants/Colors';
-import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
-
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { useRouter } from 'expo-router';
+import React, { useState } from 'react';
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 const Login = () => {
+  const router = useRouter();
+  const [isPasswordVisible, setPasswordVisible] = useState<boolean>(false);
   return (
-    <View style={{ flex: 1, backgroundColor: Colors.loginBackground }}>
-      <View
-        style={{
-          backgroundColor: Colors.loginBackgroundSecondary,
-          borderColor: Colors.acccentBlue,
-          borderWidth: 1,
-          marginTop: 10,
-          marginHorizontal: 20,
-
-          paddingBottom: '20%',
-        }}
-      >
-        <View style={{ paddingTop: '30%', alignItems: 'center' }}>
-          <Text style={{ color: 'white', fontFamily: 'Manrope', fontSize: 30 }}>
-            Welcome Back
-          </Text>
+    <View style={styles.container}>
+      <View style={styles.outerDivContainer}>
+        <View style={styles.welcomeBackTextView}>
+          <Text style={styles.welcomeBackText}>Welcome Back</Text>
         </View>
-        <View style={{ paddingTop: '3%', alignItems: 'center' }}>
-          <Text style={{ color: 'white', fontFamily: 'Manrope', fontSize: 15 }}>
+        <View style={styles.enterDetailsTextView}>
+          <Text style={styles.enterDetailsText}>
             Enter your details to access your account
           </Text>
         </View>
-        <View
-          style={{
-            backgroundColor: Colors.charcoal,
-            borderColor: Colors.acccentBlue,
-            borderWidth: 1,
-            marginTop: '3%',
-            marginHorizontal: '8%',
-            paddingBottom: '5%',
-          }}
-        >
-          <View style={{ paddingTop: '10%', paddingStart: '10%' }}>
-            <Text style={{ color: 'white', fontFamily: 'Manrope' }}>
-              EMAIL ADDRESS
-            </Text>
+        <View style={styles.innerDivContainer}>
+          <View style={styles.emailAddressView}>
+            <Text style={styles.emailAddressText}>EMAIL ADDRESS</Text>
           </View>
-          <View style={{ paddingHorizontal: '10%', paddingTop: '5%' }}>
+          <View style={styles.emailAddressTextInputView}>
+            <TextInput style={styles.emailAddressTextInputStyle} />
+          </View>
+          <View style={styles.passwordTextView}>
+            <Text style={styles.passwordText}>PASSWORD</Text>
+          </View>
+          <View style={styles.passwordTextInputView}>
             <TextInput
-              style={{
-                height: 50,
-                backgroundColor: '#BDBDBD',
-                borderRadius: 20,
-                borderColor: Colors.acccentBlue,
-                borderWidth: 0.5,
-              }}
+              maxLength={20}
+              style={styles.passwordTextInputStyle}
+              secureTextEntry={isPasswordVisible}
             />
+            <View style={styles.eyeIconAbsoluteView}>
+              <Pressable
+                style={({ pressed }) => [
+                  {
+                    opacity: pressed ? 0.5 : 1,
+                  },
+                ]}
+                onPress={() => {
+                  setPasswordVisible((prev) => !prev);
+                }}
+              >
+                <AntDesign
+                  name='eye'
+                  size={24}
+                  color='#7B8691'
+                />
+              </Pressable>
+            </View>
           </View>
-          <View style={{ paddingTop: '10%', paddingStart: '10%' }}>
-            <Text style={{ color: 'white', fontFamily: 'Manrope' }}>
-              PASSWORD
-            </Text>
+          <View style={styles.loginButtonView}>
+            <Pressable
+              style={({ pressed }) => [
+                {
+                  opacity: pressed ? 0.5 : 1,
+                },
+                styles.loginButtonStyle,
+              ]}
+            >
+              <Text style={styles.loginButtonTextStyle}>Log In</Text>
+            </Pressable>
           </View>
-          <View style={{ paddingHorizontal: '10%', paddingTop: '5%' }}>
-            <TextInput
-              style={{
-                height: 50,
-                backgroundColor: '#BDBDBD',
-                borderRadius: 20,
-                borderColor: Colors.acccentBlue,
-                borderWidth: 0.5,
-              }}
-              secureTextEntry
-            />
-          </View>
+          <Pressable
+            style={({ pressed }) => [
+              {
+                opacity: pressed ? 0.5 : 1,
+              },
+              styles.forgotPasswordTextView,
+            ]}
+          >
+            <Text style={styles.forgotPasswordText}>FORGOT PASSWORD?</Text>
+          </Pressable>
         </View>
+        <Pressable
+          onPress={() => {
+            router.replace('/(auth)/register');
+          }}
+          style={({ pressed }) => [
+            {
+              opacity: pressed ? 0.5 : 1,
+            },
+            styles.createAccountButton,
+          ]}
+        >
+          <Text style={styles.newHereText}>
+            New here?{' '}
+            <Text style={styles.createAccountText}> Create Account</Text>
+          </Text>
+        </Pressable>
       </View>
     </View>
   );
@@ -77,4 +98,81 @@ const Login = () => {
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  createAccountText: { color: 'white' },
+  newHereText: { color: '#7B8691', fontSize: 18 },
+  createAccountButton: {
+    alignItems: 'center',
+    paddingTop: '10%',
+  },
+  forgotPasswordText: { color: '#7B8691' },
+  forgotPasswordTextView: {
+    alignItems: 'center',
+    paddingTop: '10%',
+  },
+  loginButtonTextStyle: {
+    fontSize: 15,
+    fontFamily: 'Manrope',
+    fontWeight: 'bold',
+  },
+  loginButtonStyle: {
+    backgroundColor: '#FFFFFF',
+    height: 50,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 20,
+  },
+  loginButtonView: { paddingHorizontal: '10%', paddingTop: '10%' },
+  eyeIconAbsoluteView: { position: 'absolute', left: '95%', bottom: '25%' },
+  passwordTextInputStyle: {
+    height: 50,
+    backgroundColor: '#21262E',
+    borderRadius: 20,
+    borderColor: Colors.acccentBlue,
+    borderWidth: 0.5,
+    color: '#7B8691',
+    fontSize: 15,
+  },
+  passwordTextInputView: { paddingHorizontal: '10%', paddingTop: '5%' },
+  passwordText: { color: 'white', fontFamily: 'Manrope' },
+  passwordTextView: { paddingTop: '10%', paddingStart: '10%' },
+  emailAddressTextInputStyle: {
+    height: 50,
+    backgroundColor: '#21262E',
+    borderRadius: 20,
+    borderColor: Colors.acccentBlue,
+    borderWidth: 0.5,
+    color: '#7B8691',
+    fontSize: 15,
+  },
+  emailAddressTextInputView: { paddingHorizontal: '10%', paddingTop: '5%' },
+  emailAddressText: { color: 'white', fontFamily: 'Manrope' },
+  emailAddressView: { paddingTop: '10%', paddingStart: '10%' },
+  innerDivContainer: {
+    backgroundColor: '#FFFFFF1A',
+    borderColor: Colors.acccentBlue,
+    borderWidth: 0.5,
+    marginTop: '10%',
+    marginHorizontal: '8%',
+    paddingBottom: '5%',
+    borderRadius: 20,
+    shadowColor: Colors.loginBackground,
+    shadowRadius: 30,
+    shadowOffset: { width: 20, height: 20 },
+  },
+  enterDetailsText: { color: 'white', fontFamily: 'Manrope', fontSize: 15 },
+  enterDetailsTextView: { paddingTop: '3%', alignItems: 'center' },
+  welcomeBackText: { color: 'white', fontFamily: 'Manrope', fontSize: 30 },
+  welcomeBackTextView: { paddingTop: '30%', alignItems: 'center' },
+  outerDivContainer: {
+    backgroundColor: Colors.loginBackgroundSecondary,
+    borderColor: Colors.acccentBlue,
+    borderWidth: 0.5,
+    marginTop: '5%',
+    marginHorizontal: 20,
+    borderRadius: 20,
+    paddingBottom: '30%',
+  },
+  container: { flex: 1, backgroundColor: Colors.loginBackground },
+});
--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,10 +1,18 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 const Register = () => {
   return (
-    <View>
-      <Text>Register</Text>
+    <View style={{ flex: 1 }}>
+      <LinearGradient
+        // 1. Colors go from slightly more opaque to more transparent
+        colors={['#1e1b4b', '#1e1b4b']}
+        // 2. Adjust the angle (Top-Left to Bottom-Right is common for glass)
+        style={{ flex: 1 }}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+      ></LinearGradient>
     </View>
   );
 };
